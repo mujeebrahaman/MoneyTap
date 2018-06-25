ad8bc5d940fc97a36263ab735fb9c63dabf23261
mujeebrahaman <mujeeb.mjcet55@gmail.com>
2018-06-25 17:05:22 +0530

    Update Model.java

--- MoneyTap/app/src/main/java/demo/android/moneytap/com/moneytap/framework/model/Model.java
+++ MoneyTap/app/src/main/java/demo/android/moneytap/com/moneytap/framework/model/Model.java
@@ -3,9 +3,6 @@ package demo.android.moneytap.com.moneytap.framework.model;
 import java.io.Serializable;
 
 /**
- * Created by mujeeb on 26/01/17.
- */
-/**
  * Wrapper object to identify different reference.model's moving between layers
  */
 public interface Model extends Serializable {
